cb86c145cbc903e7e5218c2ae00dc0c1572e01bf
VashonG <104737326+VashonG@users.noreply.github.com>
2022-10-09 01:52:00 -0700

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,361 @@
+<?php return array (
+  'anhskohbo/no-captcha' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Anhskohbo\\NoCaptcha\\NoCaptchaServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'NoCaptcha' => 'Anhskohbo\\NoCaptcha\\Facades\\NoCaptcha',
+    ),
+  ),
+  'artesaos/seotools' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Artesaos\\SEOTools\\Providers\\SEOToolsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'SEOMeta' => 'Artesaos\\SEOTools\\Facades\\SEOMeta',
+      'OpenGraph' => 'Artesaos\\SEOTools\\Facades\\OpenGraph',
+      'Twitter' => 'Artesaos\\SEOTools\\Facades\\TwitterCard',
+      'JsonLd' => 'Artesaos\\SEOTools\\Facades\\JsonLd',
+      'SEO' => 'Artesaos\\SEOTools\\Facades\\SEOTools',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'berkayk/onesignal-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Berkayk\\OneSignal\\OneSignalServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'OneSignal' => 'Berkayk\\OneSignal\\OneSignalFacade',
+    ),
+  ),
+  'codezero/browser-locale' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'CodeZero\\BrowserLocale\\Laravel\\BrowserLocaleServiceProvider',
+    ),
+  ),
+  'codezero/dotenv-updater' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'CodeZero\\DotEnvUpdater\\Laravel\\DotEnvUpdaterServiceProvider',
+    ),
+  ),
+  'codezero/laravel-localized-routes' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'CodeZero\\LocalizedRoutes\\LocalizedRoutesServiceProvider',
+    ),
+  ),
+  'codezero/laravel-localizer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'CodeZero\\Localizer\\LocalizerServiceProvider',
+    ),
+  ),
+  'conedevelopment/i18n' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pine\\I18n\\I18nServiceProvider',
+    ),
+  ),
+  'enacton/onesignal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\OneSignal\\OneSignalServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'OneSignal' => 'EnactOn\\ProCashBee\\OneSignal\\OneSignalFacade',
+    ),
+  ),
+  'enacton/pcb-adminface' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\AdminFace\\AdminFaceProvider',
+    ),
+  ),
+  'enacton/pcb-cashback' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\Cashback\\Providers\\CashbackServiceProvider',
+    ),
+  ),
+  'enacton/pcb-commands' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\Commands\\Providers\\CommandServiceProvider',
+    ),
+  ),
+  'enacton/pcb-core' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\Core\\Providers\\CoreServiceProvider',
+    ),
+  ),
+  'enacton/pcb-customwork' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\CustomWork\\CustomWorkServiceProvider',
+      1 => 'EnactOn\\ProCashBee\\CustomWork\\CustomWorkEventServiceProvider',
+    ),
+  ),
+  'enacton/pcb-jobevents' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\JobEvents\\Providers\\JobEventServiceProvider',
+      1 => 'EnactOn\\ProCashBee\\JobEvents\\Providers\\EventServiceProvider',
+    ),
+  ),
+  'enacton/pcb-mail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\Mail\\MailServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Cashmail' => 'EnactOn\\ProCashBee\\Mail\\MailFacade',
+    ),
+  ),
+  'enacton/pcb-seo' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\SEO\\Providers\\SEOServiceProvider',
+    ),
+  ),
+  'enacton/pcb-ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'EnactOn\\ProCashBee\\UI\\Providers\\UIServiceProvider',
+      1 => 'EnactOn\\ProCashBee\\UI\\Providers\\MobileDetectServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'MobileDetect' => 'EnactOn\\ProCashBee\\UI\\Providers\\Facades\\MobileDetect',
+    ),
+  ),
+  'enacton/pcb-user-spa' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Enacton\\ProCashBee\\UserSPA\\UserSpaServiceProvider',
+    ),
+  ),
+  'facade/ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Facade\\Ignition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Facade\\Ignition\\Facades\\Flare',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'fruitcake/laravel-cors' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fruitcake\\Cors\\CorsServiceProvider',
+    ),
+  ),
+  'genealabs/laravel-model-caching' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'GeneaLabs\\LaravelModelCaching\\Providers\\Service',
+    ),
+  ),
+  'laravel-notification-channels/onesignal' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NotificationChannels\\OneSignal\\OneSignalServiceProvider',
+    ),
+  ),
+  'laravel-shift/blueprint' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Blueprint\\BlueprintServiceProvider',
+    ),
+  ),
+  'laravel/socialite' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Socialite\\SocialiteServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Socialite' => 'Laravel\\Socialite\\Facades\\Socialite',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'laravelium/sitemap' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravelium\\Sitemap\\SitemapServiceProvider',
+    ),
+  ),
+  'mad-web/laravel-initializer' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'MadWeb\\Initializer\\InitializerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/laravel-console-task' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\LaravelConsoleTask\\LaravelConsoleTaskServiceProvider',
+    ),
+  ),
+  'nunomaduro/phpinsights' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\PhpInsights\\Application\\Adapters\\Laravel\\InsightsServiceProvider',
+    ),
+  ),
+  'pavel-mironchik/laravel-backup-panel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'PavelMironchik\\LaravelBackupPanel\\LaravelBackupPanelServiceProvider',
+    ),
+  ),
+  'propaganistas/laravel-disposable-email' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Propaganistas\\LaravelDisposableEmail\\DisposableEmailServiceProvider',
+    ),
+  ),
+  'qcod/laravel-settings' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'QCod\\Settings\\SettingsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Settings' => 'QCod\\Settings\\Facade',
+    ),
+  ),
+  'realrashid/sweet-alert' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'RealRashid\\SweetAlert\\SweetAlertServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Alert' => 'RealRashid\\SweetAlert\\Facades\\Alert',
+    ),
+  ),
+  'silber/page-cache' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Silber\\PageCache\\LaravelServiceProvider',
+    ),
+  ),
+  'socialiteproviders/manager' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'SocialiteProviders\\Manager\\ServiceProvider',
+    ),
+  ),
+  'spatie/laravel-backup' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Backup\\BackupServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+);
